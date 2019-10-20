eb05a460818ebcd37701653a3b44f75b55573321
Jonathan Fuentes <fuentesyt@gmail.com>
2019-10-20 11:57:06 -0400

    Importing Project

---
+++ UNFHackathonManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UNFHackathonManagementSystem.Models;
+
+namespace UNFHackathonManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult CreateCompetition() {
+            return View();
+        }
+
+        public IActionResult JoinCompetition()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult UpdateLeaderBoard()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
